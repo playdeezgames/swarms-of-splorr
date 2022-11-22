c915834a2e82d105d1e5aaf7aec73e10b52ca504
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-22 04:58:39 -0600

    confirm quit

---
+++ src/SwarmsOfSPLORR/ConfirmProcessor.vb
@@ -0,0 +1,8 @@
+﻿Module ConfirmProcessor
+    Friend Function Run(text As String) As Boolean
+        AnsiConsole.Clear()
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = text}
+        prompt.AddChoices(NoText, YesText)
+        Return AnsiConsole.Prompt(prompt) = YesText
+    End Function
+End Module
--- src/SwarmsOfSPLORR/Constants.vb
+++ src/SwarmsOfSPLORR/Constants.vb
@@ -1,4 +1,6 @@
 ﻿Module Constants
     Friend Const OkText = "Ok"
     Friend Const QuitText = "Quit"
+    Friend Const YesText = "Yes"
+    Friend Const NoText = "No"
 End Module
