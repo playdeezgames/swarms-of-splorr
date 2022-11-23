a17aa9e9fe3d79d9bf765409d3a11baedfbd67dd
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-23 04:58:31 -0600

    enemies will head for you

--- src/SOS.Business/World.vb
+++ src/SOS.Business/World.vb
@@ -35,6 +35,32 @@ Public Class World
 
     Public Sub Start() Implements IWorld.Start
         _worldData = New WorldData
+        CreatePlayerEntity()
+        CreateEnemyEntities()
+    End Sub
+
+    Private Sub CreateEnemyEntities()
+        Const distanceStep = 10.0
+        Dim distance As Double = distanceStep
+        Dim random As New Random
+        For index = 1 To 100
+            Dim heading = random.NextDouble * Math.PI * 2
+            _worldData.Entities.Add(
+            New EntityData() With
+            {
+                .Name = $"Enemy #{index}",
+                .X = Math.Cos(heading) * distance,
+                .Y = Math.Sin(heading) * distance,
+                .Heading = 0.0,
+                .Speed = 0.0,
+                .MaximumSpeed = 1.0,
+                .SightRadius = Double.MaxValue
+            })
+            distance += distanceStep
+        Next
+    End Sub
+
+    Private Sub CreatePlayerEntity()
         _worldData.PlayerCharacterId = _worldData.Entities.Count
         _worldData.Entities.Add(
             New EntityData() With
@@ -46,17 +72,6 @@ Public Class World
                 .Speed = 1.0,
                 .MaximumSpeed = 1.0,
                 .SightRadius = 10.0
-            })
-        _worldData.Entities.Add(
-            New EntityData() With
-            {
-                .Name = "Enemy",
-                .X = 10.0,
-                .Y = 0.0,
-                .Heading = 0.0,
-                .Speed = 0.0,
-                .MaximumSpeed = 1.0,
-                .SightRadius = Double.MaxValue
             })
     End Sub
 
